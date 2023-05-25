6210f8f63d15268cd5c14c8deaf9a5d8083ccac6
Gabriela Papin <gabrielapapin20@gmail.com>
2023-05-24 22:13:16 -0300

    feat: criação de migrations

---
+++ backend/src/shared/http/typeorm/migrations/1684976501968-create-feedback.ts
@@ -0,0 +1,68 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateFeedback1684976501968 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+    await queryRunner.createTable(
+      new Table({
+        name: 'feedback',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'lastname',
+            type: 'varchar',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'house',
+            type: 'varchar',
+          },
+          {
+            name: 'family',
+            type: 'varchar',
+          },
+          {
+            name: 'stack',
+            type: 'varchar',
+          },
+          {
+            name: 'score',
+            type: 'varchar',
+          },
+          {
+            name: 'comments',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('feedback');
+  }
+}
